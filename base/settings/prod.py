4b6a2b1810091e8ff97b9f769f23f8fcf4b1b66d
JohnShahla <johnshahla19@gmail.com>
2022-06-27 14:05:09 -0400

    Finished V1.0

--- base/settings/prod.py
+++ base/settings/prod.py
@@ -7,7 +7,7 @@ django_heroku.settings(locals())
 
 DATABASES['default'] = dj_database_url.config()
 DEBUG = False
-ALLOWED_HOSTS = ['fadi-rezek-salloum.herokuapp.com']
+ALLOWED_HOSTS = ['modox-tech.herokuapp.com']
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
